887c55888871001457c4cb2d782f36c6293a5d3c
Joel Guittet <joelguittet@gmail.com>
2022-05-29 23:06:57 +0200

    wip

--- src/quizDragDropResult.js
+++ src/quizDragDropResult.js
@@ -3,12 +3,18 @@
  * This Genially script displays a Result feedback (OK/Error) depending of the location where an item is drag and dropped
  */
 
-import { glyFindParentGroupById } from "./library.js";
+/* First load and evaluate library - This import functions used in the Genially scripts */
+//import { glyFindParentGroupById } from "./library.js";
+var request = new XMLHttpRequest();
+request.onreadystatechange = function () {
+  if (request.readyState === 4 && request.status === 200) {
+    eval(request.responseText);
+  }
+};
+request.open('GET', 'https://raw.githubusercontent.com/joelguittet/genially-scripts/master/src/library.js', true);
+request.send(null);
 
-/**
- * Function called when the document is ready
- */
-//$(document).ready(function () {
-  //alert("loaded!!!");//
-  glyFindParentGroupById();
-//});
+/* Execution ofthe script */
+glyFindParentGroupById();
+
+alert("end");
