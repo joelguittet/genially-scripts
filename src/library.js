fee55db26529c744e8189ba1c243d7c8e8b90cf3
Joel Guittet <joelguittet@gmail.com>
2022-05-29 21:58:46 +0200

    quiz: create drag drop result

---
+++ src/library.js
@@ -0,0 +1,8 @@
+/**
+ * Library
+ * Contain common function to be used in other Genially scripts
+ */
+
+export function glyFindParentGroup(name) {
+  alert("test!");
+}
---
+++ src/quizDragDropResult.js
@@ -0,0 +1,13 @@
+/**
+ * Quiz - Drag Drop and Result
+ * This Genially script displays a Result feedback (OK/Error) depending of the location where an item is drag and dropped
+ */
+
+import { glyFindParentGroup } from "./library.js";
+
+/**
+ * Function called when the document is ready
+ */
+$(document).ready(function () {
+  glyFindParentGroup();
+});
