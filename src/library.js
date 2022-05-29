03e5e2e6eae23e0cba90eb3f56c5f9ae4fa47575
Joel Guittet <joelguittet@gmail.com>
2022-05-29 22:38:19 +0200

    drag drop and result: wip

--- src/library.js
+++ src/library.js
@@ -3,6 +3,27 @@
  * Contain common function to be used in other Genially scripts
  */
 
-export function glyFindParentGroup(name) {
-  alert("test!");
+/**
+ * Find parent group with script id
+ * This function is used to retrieve a group done in Genially using the id of the script element attached to it
+ * @param id ID of the script element attached to the group
+ * @return Group of the wanted element, null if not found
+ */
+export function glyFindParentGroupById(id) {
+
+  var oObject = document.getElementById(id);
+  for (let i = 0; i < 15; i++) {
+    if (oObject.getAttribute("id") == "canva-container") {
+      /* Unableto find the group, maybe we are in edition mode ? */
+      return null;
+    }
+    if ((oObject.getAttribute("id") == null) || (oObject.getAttribute("id").search("genially-animated") < 0)) {
+      /* Go to parent element */
+      oObject = oObject.parentNode;
+    } else {
+      /* Found the wanted element */
+      break;
+    }
+  }
+  return oObject;
 }
--- src/quizDragDropResult.js
+++ src/quizDragDropResult.js
@@ -3,11 +3,11 @@
  * This Genially script displays a Result feedback (OK/Error) depending of the location where an item is drag and dropped
  */
 
-import { glyFindParentGroup } from "./library.js";
+//import { glyFindParentGroupById } from "./library.js";
 
 /**
  * Function called when the document is ready
  */
 $(document).ready(function () {
-  glyFindParentGroup();
+  alert("loaded!!!");//glyFindParentGroupById();
 });
