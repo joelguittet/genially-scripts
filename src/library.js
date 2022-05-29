44be57f1dc271d96894a6e22d7c5a228fb75ec27
Joel Guittet <joelguittet@gmail.com>
2022-05-29 23:08:18 +0200

    wip

--- src/library.js
+++ src/library.js
@@ -9,7 +9,7 @@
  * @param id ID of the script element attached to the group
  * @return Group of the wanted element, null if not found
  */
-export function glyFindParentGroupById(id) {
+function glyFindParentGroupById(id) {
 alert("try to find");
   var oObject = document.getElementById(id);
   for (let i = 0; i < 15; i++) {
