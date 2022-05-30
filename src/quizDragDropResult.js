6d799e3034fd1d40972883971bf45521119740ff
Joel Guittet <joelguittet@gmail.com>
2022-05-30 19:49:21 +0200

    wip

--- src/quizDragDropResult.js
+++ src/quizDragDropResult.js
@@ -3,7 +3,7 @@
  * This Genially script displays a Result feedback (OK/Error) depending of the location where an item is drag and dropped
  */
 
-import { glyFindParentGroupById } from "https://raw.githubusercontent.com/joelguittet/genially-scripts/master/src/library.js";
+//import { glyFindParentGroupById } from "https://raw.githubusercontent.com/joelguittet/genially-scripts/master/src/library.js";
 
 //function main()
 //  glyFindParentGroupById();
@@ -11,16 +11,14 @@ import { glyFindParentGroupById } from "https://raw.githubusercontent.com/joelgu
 //}
 
 /* First load and evaluate library - This import functions used in the Genially scripts */
-/*var request = new XMLHttpRequest();
+var request = new XMLHttpRequest();
 request.onreadystatechange = function () {
   if (request.readyState === 4 && request.status === 200) {
     eval(request.responseText);
-    main();
-  }
-};
-request.open('GET', 'https://raw.githubusercontent.com/joelguittet/genially-scripts/master/src/library.js', true);
-request.send(null);*/
-
 alert("begin ok");
 glyFindParentGroupById("id_test");
 alert("end ok");
+  }
+};
+request.open('GET', 'https://raw.githubusercontent.com/joelguittet/genially-scripts/master/src/library.js', true);
+request.send(null);
