288bee18599f2bb378359b410494614f16faa6b1
Joel Guittet <joelguittet@gmail.com>
2022-05-30 22:14:22 +0200

    wip quiz

--- src/quizDragDropResult.js
+++ src/quizDragDropResult.js
@@ -1,10 +1,35 @@
 /**
  * Quiz - Drag Drop and Result
  * This Genially script displays a Result feedback (OK/Error) depending of the location where an item is drag and dropped
- * Item name must have ID "O1" and the destinations must have ID "Z1", "Z2", etc.
- * The result items to be displayed have ID "OK" and "Error" (The Error item is optional)
+ * Draggable item must have ID "O1" and the destinations zones must have ID "Z1", "Z2", etc.
+ * The result items to be displayed have ID "OK" and "Error" (the "Error" item is optional), "OK" item is displayed if "O1" is near to "Z1", "Error" item is displayed if "O1" is near to another zone
  */
 
-alert("begin ok");
-glyFindParentGroupById("id_test");
-alert("end ok");
+/* Listen for mouse 'up' event */
+docOnMouseUp(function() {
+
+  /* Find draggable item */
+  var oObject = glyFindParentGroupById("O1");
+  if (oObject == null) {
+    return;
+  }
+  
+  /* Parse all destination zones */
+  var oDestinationZone;
+  var nDestinationIndex = 1;
+  do {
+  
+    /* Get current desination zone */
+    oDestinationZone = glyFindParentGroupById("Z" + nDestinationIndex);
+    if (oDestinationZone != null) {alert("find " + "Z" + nDestinationIndex);
+    
+      /* Check if the draggable item is near to the current zone */
+      if (glyCheckProximity(oObject, oDestinationZone)) {alert("they are closed");
+      
+      
+      
+      }
+    }
+    
+  } while (oDestinationZone != null);
+});
